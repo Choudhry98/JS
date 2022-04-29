d1e83592d6c51c9445793a9d549d26f827cd7fb2
Choudhry98 <102037816+Choudhry98@users.noreply.github.com>
2022-04-29 12:53:15 +0530

    Add files via upload

---
+++ js day 4/index.js
@@ -0,0 +1,31 @@
+//console.log("working");
+
+//for(let i=0;i<10;i++){
+ //   console.log(i)
+//}
+
+
+//let a=20;
+//while(a>0){
+//console.log(a);
+//a--;
+//}
+
+
+//for(let a=1;a<=10;a++){
+  //  console.log(a*2)
+//}
+
+//let i=1
+//while(i<=10){
+  //  console.log(i*2);
+   // i++;
+//}
+
+
+
+let x=2;
+do{
+    console.log(x);
+    x=x+2;
+}while(x<=20);
